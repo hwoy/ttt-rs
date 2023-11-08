79eface14d28b4610657cc537730944c4c27aa43
Watthanachai Dueanklang <bosskillerz@gmail.com>
2023-11-08 11:04:54 +0700

    xxx

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(non_camel_case_types)]
+#![allow(dead_code)]
 
 extern crate ttt_sys;
 
@@ -16,67 +17,80 @@ impl ox_player_impl for ttt_sys::ox_player {
 
 use std::mem::MaybeUninit;
 pub struct Builder {
-    game: ttt_sys::ox_game,
+    game: MaybeUninit<ttt_sys::ox_game>,
 }
 
 impl Builder {
-    pub fn new(id: c_uint, seed: c_uint) -> Self {
-        let mut s = unsafe { ttt_sys::ox_creatgame(seed) };
-        s.id = id;
-        Self { game: s }
+    pub fn new() -> Self {
+        Self {
+            game: MaybeUninit::uninit(),
+        }
     }
+	
+	fn as_game(&self)->&ttt_sys::ox_game
+	{
+		unsafe { self.game.assume_init_ref() }
+	}
+	
+	fn as_game_mut(&mut self)->&mut ttt_sys::ox_game
+	{
+		unsafe { self.game.assume_init_mut() }
+	}
 
     pub fn set_id(mut self, id: c_uint) -> Self {
-        self.game.id = id;
+        let self_game = self.as_game_mut();
+        self_game.id = id;
         self
     }
 
     pub fn set_seed(mut self, seed: c_uint) -> Self {
+        let self_game = self.as_game_mut();
         unsafe {
-            ttt_sys::glibcrnginit(self.game.random.as_mut_ptr() as *mut ttt_sys::RND32, seed);
+            ttt_sys::glibcrnginit(self_game.random.as_mut_ptr() as *mut ttt_sys::RND32, seed);
         }
         self
     }
 
     pub fn set_win(mut self, win: &[::std::os::raw::c_uint; 8usize]) -> Self {
-        self.game.win = *win;
+        let self_game = self.as_game_mut();
+        self_game.win = *win;
         self
     }
     pub fn set_tri(mut self, tri: &[::std::os::raw::c_uint; 48usize]) -> Self {
-        self.game.tri = *tri;
+        let self_game = self.as_game_mut();
+        self_game.tri = *tri;
         self
     }
     pub fn set_nwin(mut self, nwin: c_uint) -> Self {
-        self.game.nwin = nwin;
+        let self_game = self.as_game_mut();
+        self_game.nwin = nwin;
         self
     }
     pub fn set_ntri(mut self, tri: c_uint) -> Self {
-        self.game.ntri = tri;
+        let self_game = self.as_game_mut();
+        self_game.ntri = tri;
         self
     }
     pub fn set_nelement(mut self, nelement: c_uint) -> Self {
-        self.game.nelement = nelement;
+        let self_game = self.as_game_mut();
+        self_game.nelement = nelement;
         self
     }
     pub fn set_ntrielement(mut self, ntrielement: c_uint) -> Self {
-        self.game.ntrielement = ntrielement;
+        let self_game = self.as_game_mut();
+        self_game.ntrielement = ntrielement;
         self
     }
     pub fn build(self) -> ttt_sys::ox_game {
-        self.game
+        unsafe { self.game.assume_init() }
     }
 }
 
 pub trait ox_game_impl {
-    fn new(id: c_uint, seed: c_uint) -> Self;
 }
 
 impl ox_game_impl for ttt_sys::ox_game {
-    fn new(id: c_uint, seed: c_uint) -> Self {
-        let mut s = unsafe { ttt_sys::ox_creatgame(seed) };
-        s.id = id;
-        s
-    }
+
 }
 
 /*****************************************************************/
